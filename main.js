ac9630bc89c0d626d3513351bd7cfcfc5069ecce
windsnow1025 <windsnow1025@gmail.com>
2024-04-19 13:25:03 -0700

    Initial commit

---
+++ main.js
@@ -0,0 +1,26 @@
+const { app, BrowserWindow } = require('electron')
+
+const createWindow = () => {
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+    }
+  })
+
+  mainWindow.loadURL("http://localhost:3000")
+
+  // mainWindow.webContents.openDevTools()
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+})
