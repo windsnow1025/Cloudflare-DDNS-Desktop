0e5b023004f3d6a13e262bade055e2c5de1f468e
windsnow1025 <windsnow1025@gmail.com>
2024-04-20 15:33:26 -0700

    chore: remove markdown highlight theme

--- renderer/app/components/utils/Theme.ts
+++ renderer/app/components/utils/Theme.ts
@@ -14,8 +14,6 @@ const convertTheme = (systemTheme: string) => {
 export function applyTheme(systemTheme: string) {
   const theme = convertTheme(systemTheme);
   applyMainTheme(theme);
-  applyMarkdownTheme(theme);
-  applyHighlightTheme(theme);
 }
 
 function applyMainTheme(theme: string) {
@@ -29,64 +27,6 @@ function applyMainTheme(theme: string) {
     body.classList.add("light-theme");
   } else if (theme === "dark") {
     body.classList.add("dark-theme");
-  }
-}
-
-function applyMarkdownTheme(theme: string) {
-  // Get all link elements
-  const links = document.getElementsByTagName('link');
-
-  // Loop through all link elements
-  for (let i = 0; i < links.length; i++) {
-    const link = links[i];
-    const href = link.getAttribute('href');
-
-    // If link is for github-markdown-css, remove it
-    if (href && href.includes('/markdown/')) {
-      link.parentNode?.removeChild(link);
-    }
-  }
-
-  // Add new link element
-  if (theme === 'dark') {
-    const darkCss = document.createElement('link');
-    darkCss.setAttribute('rel', 'stylesheet');
-    darkCss.setAttribute('href', '/css/markdown/github-markdown-dark.css');
-    document.head.appendChild(darkCss);
-  } else if (theme === 'light') {
-    const lightCss = document.createElement('link');
-    lightCss.setAttribute('rel', 'stylesheet');
-    lightCss.setAttribute('href', '/css/markdown/github-markdown-light.css');
-    document.head.appendChild(lightCss);
-  }
-}
-
-function applyHighlightTheme(theme: string) {
-  // Get all link elements
-  const links = document.getElementsByTagName('link');
-
-  // Loop through all link elements
-  for (let i = 0; i < links.length; i++) {
-    const link = links[i];
-    const href = link.getAttribute('href');
-
-    // If link is for highlight.js, remove it
-    if (href && href.includes('highlight')) {
-      link.parentNode?.removeChild(link);
-    }
-  }
-
-  // Add new link element
-  if (theme === 'dark') {
-    const darkCss = document.createElement('link');
-    darkCss.setAttribute('rel', 'stylesheet');
-    darkCss.setAttribute('href', '/css/highlight/github-dark.css');
-    document.head.appendChild(darkCss);
-  } else if (theme === 'light') {
-    const lightCss = document.createElement('link');
-    lightCss.setAttribute('rel', 'stylesheet');
-    lightCss.setAttribute('href', '/css/highlight/github.css');
-    document.head.appendChild(lightCss);
   }
 }
 
