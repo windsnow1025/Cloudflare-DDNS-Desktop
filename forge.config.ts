282d2b8e9d68b939030512ecef4ee128daf0f17e
windsnow1025 <windsnow1025@gmail.com>
2026-04-02 04:36:38 -0400

    feat: icon

--- forge.config.ts
+++ forge.config.ts
@@ -10,6 +10,7 @@ import { FuseV1Options, FuseVersion } from '@electron/fuses';
 const config: ForgeConfig = {
   packagerConfig: {
     asar: true,
+    icon: 'resources/icon',
   },
   rebuildConfig: {},
   makers: [
--- src/main.ts
+++ src/main.ts
@@ -12,6 +12,7 @@ const createWindow = () => {
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
+    icon: path.join(app.getAppPath(), 'resources/icon.ico'),
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
       webSecurity: false,
